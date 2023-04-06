44a834dfca653ed0ae50f7343b76ae9a0301fe1c
Bernardo Esbérard <bernardbr@users.noreply.github.com>
2023-04-06 20:56:18 -0300

    Update delete-stale-branches.sh

--- delete-stale-branches.sh
+++ delete-stale-branches.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Asks user confirmation to proceed
-read -p "This command will delete all local branches that no longer exist in the remote repository. Do you wish to continue? ([Y]es/[N]o/[A]ll) " confirm
+while true; do
+  read -p "This command will delete all local branches that no longer exist in the remote repository. Do you wish to continue? ([Y]es/[N]o/[A]ll) " confirm
+  if [[ "$confirm" == "Y" || "$confirm" == "y" || "$confirm" == "N" || "$confirm" == "n" || "$confirm" == "A" || "$confirm" == "a" ]]; then
+    break
+  else
+    echo "Invalid input. Please enter a valid option."
+  fi
+done
+
 if [[ "$confirm" == "N" || "$confirm" == "n" ]]; then
   echo "Operation canceled."
   exit 1
@@ -20,7 +28,14 @@ for branch in $local_branches; do
       git branch -D "$branch"
       echo "Local branch \"$branch\" deleted."
     else
-      read -p "Do you want to delete local branch \"$branch\"? (Y/N) " confirm_delete
+      while true; do
+        read -p "Do you want to delete local branch \"$branch\"? (Y/N) " confirm_delete
+        if [[ "$confirm_delete" == "Y" || "$confirm_delete" == "y" || "$confirm_delete" == "N" || "$confirm_delete" == "n" ]]; then
+          break
+        else
+          echo "Invalid input. Please enter a valid option."
+        fi
+      done
       if [[ "$confirm_delete" == "Y" || "$confirm_delete" == "y" ]]; then
         git branch -D "$branch"
         echo "Local branch \"$branch\" deleted."
